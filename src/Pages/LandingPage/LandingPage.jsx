91d1a0df39ac1381980175d90faa0103de5151eb
obafemi <oludahunsiobafemi18@gmail.com>
2022-06-22 10:13:28 +0100

    created the navigation bar and styled it using th styled component

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,62 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import logo from "../../assets/images/login-logo.svg";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <Container>
+      <Nav>
+        <Logo>
+          <Link to="/">
+            <img src={logo} alt="linkedin-logo" />
+          </Link>
+        </Logo>
+        <AccessLink>
+          <Join> Join now </Join>
+          <Login> Sign in </Login>
+        </AccessLink>
+      </Nav>
+    </Container>
+  );
 };
 
+const Container = styled.div`
+  width: 100%;
+`;
+
+const Nav = styled.nav`
+  padding: 5px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled.div`
+  width: 100px;
+`;
+const AccessLink = styled.div`
+  display: flex;
+`;
+const Join = styled.div`
+  padding: 10px 18px;
+  font-family: sans-serif;
+  font-weight: 600;
+  color: #73777b;
+  &:hover {
+    background-color: #dfdfde;
+    border-radius: 40px;
+    cursor: pointer;
+  }
+`;
+const Login = styled.div`
+  margin-left: 10px;
+  padding: 10px 18px;
+  color: #0072b1;
+  border: 1px solid #0072b1;
+  border-radius: 40px;
+  font-weight: 600;
+  font-family: sans-serif;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 export default LandingPage;
