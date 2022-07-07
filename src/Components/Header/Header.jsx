ee6672982921d715b3a43d27e9ac89bc40c5a07e
obafemi <oludahunsiobafemi18@gmail.com>
2022-07-07 08:11:36 +0100

    installed the redux and react-redux. Created the redux folder and the user slice

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,12 +8,13 @@ import messaging from "../../assets/images/nav-messaging.svg";
 import jobs from "../../assets/images/nav-jobs.svg";
 import network from "../../assets/images/nav-network.svg";
 import home from "../../assets/images/nav-home.svg";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <Container>
       <Content>
-        <RightSection>
+        <LeftSection>
           <Logo>
             <img src={logo} alt="" />
           </Logo>
@@ -23,35 +24,60 @@ const Header = () => {
               <img src={searchIcon} alt="" />
             </SearchIcon>
           </Search>
-        </RightSection>
-        <LeftSection>
+        </LeftSection>
+        <RightSection>
           <NavItemsList>
-            <NavItem>
-              <img src={home} alt="home-icon" />
-              <p>Home</p>
-            </NavItem>
-            <NavItem>
-              <img src={network} alt="network-icon" />
-              <p>My Network</p>
-            </NavItem>
-            <NavItem>
-              <img src={jobs} alt="job-icon" />
-              <p>Jobs</p>
-            </NavItem>
-            <NavItem>
-              <img src={messaging} alt="messaging-icon" />
-              <p>Messaging</p>
-            </NavItem>
-            <NavItem>
-              <img src={notification} alt="notification-icon" />
-              <p>Notifications</p>
-            </NavItem>
-            <WorkMenu>
+            <NavLink
+              to="/home"
+              className={({ isActive }) => (isActive ? "active " : "")}
+            >
+              <NavItem>
+                <img src={home} alt="home-icon" />
+                <span>Home</span>
+              </NavItem>
+            </NavLink>
+            <NavLink
+              to="/network"
+              className={({ isActive }) => (isActive ? "active " : "")}
+            >
+              <NavItem>
+                <img src={network} alt="network-icon" />
+                <span>My Network</span>
+              </NavItem>
+            </NavLink>
+            <NavLink
+              to="/jobs"
+              className={({ isActive }) => (isActive ? "active " : "")}
+            >
+              <NavItem>
+                <img src={jobs} alt="job-icon" />
+                <span>Jobs</span>
+              </NavItem>
+            </NavLink>
+            <NavLink
+              to="/messages"
+              className={({ isActive }) => (isActive ? "active " : "")}
+            >
+              <NavItem>
+                <img src={messaging} alt="messaging-icon" />
+                <span>Messaging</span>
+              </NavItem>
+            </NavLink>
+            <NavLink
+              to="/notifications"
+              className={({ isActive }) => (isActive ? "active " : "")}
+            >
+              <NavItem>
+                <img src={notification} alt="notification-icon" />
+                <span>Notifications</span>
+              </NavItem>
+            </NavLink>
+            {/* <WorkMenu>
               <img src={menu} alt="menu-icon" />
               <p>Work</p>
-            </WorkMenu>
+            </WorkMenu> */}
           </NavItemsList>
-        </LeftSection>
+        </RightSection>
       </Content>
     </Container>
   );
@@ -67,13 +93,13 @@ const Content = styled.div`
   max-width: 1128px;
   margin: auto;
   display: flex;
+  min-height: 100%;
   padding: 8px 20px;
   align-items: center;
 `;
-const RightSection = styled.div`
+const LeftSection = styled.div`
   display: flex;
   align-items: center;
-  /* background-color: blue; */
 `;
 const Logo = styled.div`
   width: 30px;
@@ -83,7 +109,7 @@ const Logo = styled.div`
 `;
 const Search = styled.div`
   width: 200px;
-  position: relative;
+  /* position: relative; */
   input {
     width: 100%;
     height: 25px;
@@ -97,9 +123,6 @@ const Search = styled.div`
     &::placeholder {
       font-weight: 300;
     }
-    @media (max-width: 768px) {
-      display: none;
-    }
   }
 `;
 const SearchIcon = styled.div`
@@ -111,57 +134,88 @@ const SearchIcon = styled.div`
   justify-content: center;
   margin: auto;
 `;
-const LeftSection = styled.nav`
-  /* background-color: red; */
+const RightSection = styled.nav`
+  display: block;
   margin-left: auto;
   @media (max-width: 768px) {
-    position: absolute;
+    position: fixed;
     right: 20px;
-    /* bottom: 0;
-    top: auto; */
+    background-color: white;
+    display: flex;
+    width: 100vw;
+    left: 0;
+    right: 0;
+    bottom: 0;
   }
 `;
 const NavItemsList = styled.ul`
   list-style-type: none;
   display: flex;
-  column-gap: 30px;
+  flex-wrap: nowrap;
+  column-gap: 5px;
   @media (max-width: 768px) {
-    column-gap: 25px;
+    width: 100vw;
+    align-items: center;
+    justify-content: center;
+    padding: 5px 5px;
+    /* justify-content: space-evenly; */
   }
+  a {
+    text-decoration: none;
+  }
+  /* .active {
+    span:after {
+      transform: scaleX(1);
+      border-bottom: 2px solid var(--white, #fff);
+      position: absolute;
+      bottom: 0;
+      transition: transform 0.2s ease-in-out;
+      border-color: rgba(0, 0, 0, 0.9);
+    }
+  } */
 `;
 const NavItem = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  p {
+  padding: 0px 10px;
+  background-color: red;
+
+  span {
     font-family: sans-serif;
     font-weight: 300;
-    color: #5b5b5b;
-    font-size: 12px;
+    color: rgba(0, 0, 0, 0.6);
+    font-size: 11px;
     padding-top: 5px;
-    @media (max-width: 768px) {
-      display: none;
-    }
   }
-  &:hover {
+  &:hover,
+  &:active {
     cursor: pointer;
-  }
-`;
-const WorkMenu = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 15px;
-  border-left: 1px solid lightgray;
-  p {
-    font-family: sans-serif;
-    font-weight: 300;
-    color: #5b5b5b;
-    font-size: 12px;
-    padding-top: 2px;
-    @media (max-width: 768px) {
-      display: none;
+    span {
+      color: rgba(0, 0, 0, 0.9);
     }
   }
+  @media (max-width: 768px) {
+    width: 100%;
+    /* padding: 0px px; */
+  }
 `;
+
+// const WorkMenu = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   padding-left: 15px;
+//   border-left: 1px solid lightgray;
+//   p {
+//     font-family: sans-serif;
+//     font-weight: 300;
+//     color: #5b5b5b;
+//     font-size: 12px;
+//     padding-top: 2px;
+//     @media (max-width: 768px) {
+//       display: none;
+//     }
+//   }
+// `;
 export default Header;
